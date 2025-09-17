2aedbea6d996e153a058a0ddc3987b103efa7c30
Ivan Grishaev <ivan@grishaev.me>
2025-09-17 18:38:37 +0300

    updated

--- src/any/core.clj
+++ src/any/core.clj
@@ -1,7 +1,4 @@
 (ns any.core
-  (:import
-   (java.time LocalDate)
-   (java.io Writer))
   (:require
    [clojure.string :as str])
   (:refer-clojure :exclude [uuid int]))
@@ -19,7 +16,7 @@
              ~@(when repr
                  [`(toString [~this] ~repr)]))]
        (defmethod print-method (type result#)
-         [x# ^Writer w#]
+         [x# ^java.io.Writer w#]
          (.write w# ^String (str x#)))
        result#)))
 
@@ -28,28 +25,89 @@
      (contains? #{~@items} x#)))
 
 (defmacro instance [Class]
-  `(any [x# (format "<instance of %s>" (.getName ~Class))]
+  `(any [x# (format "<instance: %s>" (.getName ~Class))]
      (instance? ~Class x#)))
+
+(defmacro pred [fn-name]
+  `(any [x# (format "<pred: %s>" ~(str fn-name))]
+     (~fn-name x#)))
 
 ;; pred
 ;; range
 
+(def LocalDate
+  (instance java.time.LocalDate))
 
-(defmacro defany [name [other repr] & body]
-  `(def ~name (any [~other ~repr] ~@body)))
+(def LocalDateTime
+  (instance java.time.LocalDateTime))
+
+(def LocalTime
+  (instance java.time.LocalTime))
+
+(def OffsetTime
+  (instance java.time.OffsetTime))
+
+(def OffsetDateTime
+  (instance java.time.OffsetDateTime))
+
+(def Instant
+  (instance java.time.Instant))
+
+(def ZonedDateTime
+  (instance java.time.ZonedDateTime))
+
+(def Period
+  (instance java.time.Period))
+
+(def File
+  (instance java.io.File))
+
+(def InputStream
+  (instance java.io.InputStream))
+
+(def OutputStream
+  (instance java.io.OutputStream))
+
+(def Reader
+  (instance java.io.Reader))
+
+(def Writer
+  (instance java.io.Writer))
+
+(def Date
+  (instance java.util.Date))
+
+(def UUID
+  (instance java.util.UUID))
 
 #_
-(defany int [x "any int"]
-  (cc/int? x))
+(intern 'any.core
+        'Number
+        (instance java.lang.Number))
 
-(defany uuid [x "any uuid"]
-  (cc/uuid? x))
+#_
+(intern 'any.core
+        'String
+        (instance java.lang.String))
 
-(defany uuid [x "any keyword"]
-  (cc/keyword? x))
 
-(defany local-date [x "any LocalDate"]
-  (instance? LocalDate x))
+
+
+;; (defmacro defany [name [other repr] & body]
+;;   `(def ~name (any [~other ~repr] ~@body)))
+
+;; #_
+;; (defany int [x "any int"]
+;;   (cc/int? x))
+
+;; (defany uuid [x "any uuid"]
+;;   (cc/uuid? x))
+
+;; (defany uuid [x "any keyword"]
+;;   (cc/keyword? x))
+
+;; (defany local-date [x "any LocalDate"]
+;;   (instance? LocalDate x))
 
 #_
 (= {:foo (enum :fo :bar :baz)} {:foo :sdfsdf})
